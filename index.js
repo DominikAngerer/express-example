20bfd40663d0de5e7fd5b62c0e49fc7a7e19da5e
DominikAngerer <me@dominikangerer.com>
2017-08-15 21:24:05 +0200

    adds globalData

--- index.js
+++ index.js
@@ -25,17 +25,30 @@ app.get('/*', function(req, res) {
   path = path == '/' ? 'home' : path; // this will allow to visit "home" from / only but still allow the editor to access the home "page" on localhost:4300/home
 
   Storyblok
-    .get(`stories/${path}`, { // dynamic variable also here!
+    .get(`stories/global`, { // load the global Story upfront so we can pass it in line 41
       version: req.query._storyblok ? 'draft': 'published'
     })
-    .then((response) => {
-      res.render(response.body.story.content.component, { // changed to dynamic "base" component -> Since you've created 2 components "home" and "about" in your components overview: http://app.storyblok.com/#!/me/spaces/40936/components/ those are also "root" components and therefor can define another layout! :)
-        story: response.body.story
+    .then((globalResponse) => {
+      let globalData = globalResponse.body.story.content;
+    
+      Storyblok
+      .get(`stories/${path}`, { // dynamic variable also here!
+        version: req.query._storyblok ? 'draft': 'published'
+      })
+      .then((response) => {
+        res.render(response.body.story.content.component, { // changed to dynamic "base" component -> I've created 2 components "home" and "about" -> Have a look in your components overview: http://app.storyblok.com/#!/me/spaces/40936/components/ 
+          story: response.body.story,
+          global: globalData // pass the global content to Handlebars
+        });
+      })
+      .catch((error) => {
+        res.send(error);
       });
+
     })
-    .catch((error) => {
-      res.send(error);
-    });
+  .catch((error) => {
+    res.send(error);
+  });
 });
 
 
